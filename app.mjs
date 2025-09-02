d0e5b85cb8c4f20928aba704d201bea75f3f5f10
RovinKYK <kykrovin@gmail.com>
2025-09-02 17:38:58 +0530

    Allow cors

--- app.mjs
+++ app.mjs
@@ -1,8 +1,11 @@
 import express from "express";
 import cache from "./cache.mjs";
 import { v4 as uuidv4 } from "uuid";
+import cors from "cors";
 
 const app = express();
+// Enable CORS for all routes
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
